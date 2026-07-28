1d6cc8468d89ca936d3260dcef0a9eb3e53a5d99
Theo Farias <tfarias@csumb.edu>
2026-07-27 22:08:06 -0700

    Added SceneType enum for use in SceneFactory

---
+++ src/main/java/SceneType.java
@@ -0,0 +1,13 @@
+/**
+ * enums to define scene types that can be created.
+ *
+ * @author Theodore Farias
+ * @version 0.1.0
+ * @since 7/27/2026
+ */
+public enum SceneType {
+  // Place commas between enums and semicolon after last.
+  LOGIN,
+  CREATE_ACCOUNT,
+  PLAYER_ACCOUNT;
+}
