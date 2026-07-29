d5939777dd992610a9a1e9a97d6bcf8a2a32216c
Theo Farias <tfarias@csumb.edu>
2026-07-28 22:32:20 -0700

    Completed database setup, use SQL_Bank to add your sql table info.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
 
+    DatabaseManager.getInstance();
     primaryStage.setTitle("Monstruos");
     primaryStage.show();
   }
---
+++ src/main/java/SQL_Bank.java
@@ -0,0 +1,58 @@
+/**
+ * Enum for SQL Strings.
+ *
+ * @author Theodore Farias
+ * @version 0.1.0
+ * @since 7/28/2026
+ */
+
+public enum SQL_Bank {
+  PLAYER("player", """
+      CREATE TABLE IF NOT EXISTS player (
+          id          INTEGER PRIMARY KEY AUTOINCREMENT,
+          username    TEXT NOT NULL,
+          password    TEXT NOT NULL,
+          name        TEXT NOT NULL,
+          character   INTEGER NOT NULL DEFAULT 0,
+          monstruos   TEXT DEFAULT NULL,
+          level       INTEGER NOT NULL DEFAULT 0,
+          health      REAL NOT NULL DEFAULT 100.0
+      )
+      """);
+
+  // Add next table enum here
+
+  // Name of table
+  private final String name;
+  // String of sql create statement
+  private final String sql;
+
+  /**
+   * Constructor for SQL_Bank enum.
+   *
+   * @param name name of table
+   * @param sql  table create statement.
+   */
+  SQL_Bank(String name, String sql) {
+    this.name = name;
+    this.sql = sql;
+  }
+
+  /**
+   * sql getter
+   *
+   * @return sql String.
+   */
+  public String getSql() {
+    return sql;
+  }
+
+  /**
+   * name getter
+   *
+   * @return table name String
+   */
+  public String getName() {
+    return name;
+  }
+}
