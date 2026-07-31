5448ffa135d0c4923bdefcff73770ac596ca2046
Theo Farias <tfarias@csumb.edu>
2026-07-31 06:36:51 -0700

    Updated SceneFactory to final class. Simplified create method. Added javadoc comments.

--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -8,42 +8,52 @@ import javafx.stage.Stage;
  * @version 0.1.0
  * @since 7/30/2026
  */
-public interface SceneFactory {
+public final class SceneFactory {
 
   /**
    * Chooses appropriate scene builder method based on provided SceneType.
    *
    * @param sceneType enum for scene types
-   * @param stage primary stage
+   * @param stage     primary stage
    * @return scene build by scene builder method.
    */
   public static Scene create(SceneType sceneType, Stage stage) {
-    Scene scene = null;
-    switch (sceneType) {
-      case LOGIN -> {
-        scene = buildLogin(stage);
-      }
-      case CREATE_ACCOUNT -> {
-        scene = buildCreateAccount(stage);
-      }
-      case PLAYER_ACCOUNT -> {
-        scene = buildPlayerAccount(stage);
-      }
-    }
-    return scene;
+    return switch (sceneType) {
+      case LOGIN -> buildLogin(stage);
+      case CREATE_ACCOUNT -> buildCreateAccount(stage);
+      case PLAYER_ACCOUNT -> buildPlayerAccount(stage);
+    };
   }
 
+  /**
+   * Builds Login scene
+   *
+   * @param stage primaryStage
+   * @return Login scene
+   */
   private static Scene buildLogin(Stage stage) {
     DatabaseManager.getInstance();
     System.out.println("Not yet implemented in FXML");
     return null;
   }
 
+  /**
+   * Builds Create Account scene
+   *
+   * @param stage primaryStage
+   * @return Create Account scene
+   */
   private static Scene buildCreateAccount(Stage stage) {
     System.out.println("Not yet implemented in FXML");
     return null;
   }
 
+  /**
+   * Builds Player Account scene
+   *
+   * @param stage primaryStage
+   * @return Player Account scene
+   */
   private static Scene buildPlayerAccount(Stage stage) {
     System.out.println("Not yet implemented in FXML");
     return null;
